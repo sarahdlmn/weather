b5eaa5eb48855b6eb9039da92c0d1b2f10b2c992
Sarah <sarahdalimonte@gmail.com>
2020-02-18 17:00:58 +0100

    feat(weather): add icon, description, temp

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {mockWeather} from '../mocks/mockWeather';
  
 function Weather() {
+
+    const [weather, setWeather] = useState(mockWeather);
+
+    function kelvinToCelsius(tempKelvin) {
+        return Math.round(tempKelvin -273.15);
+    }
+
+    function loadIconWeather(idIcon){
+        return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png"
+    }
     return (
         <div>
-            
+            <h1>Météo : {weather.name}</h1>
+            <img src={loadIconWeather(weather.weather[0].icon)} alt=""/>
+            <p>{weather.weather[0].description}</p>
+            <p>{kelvinToCelsius(weather.main.temp)} C°</p>
         </div>
     )
 }
